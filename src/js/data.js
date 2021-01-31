2e7a0dd6b51aa7bac4f19566484f97200527e1f1
Egi <egimalgina@gmail.com>
2021-01-31 23:19:20 +0700

    Mengatur style halaman surah

--- src/js/data.js
+++ src/js/data.js
@@ -3,34 +3,31 @@ $.ajax ({
    
     success : results => {
         console.log(results)
-        const quran = results.ayt;
+        const quran = results.surah.ayat;
         console.log(quran)
         let qcard = '';
         quran.forEach( q => {
-            qcard +=`<div class="container  my-5">
-                        <p>
-                            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                            Button with data-bs-target
-                            </button>
-                            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">
-                                Button with data-bs-target
-                            </button>
-                        </p>
-                        <div class="collapse show" id="collapseExample">
-                            <div class="card card-body">
-                            ${q.text}
-                            </div>
+            qcard +=`<div class="container  my-3">
+                        <div class="btn-group" role="group" aria-label="Basic">
+                            <button type="button" class="btn btn-1 rounded-0">${q.id_surah} : ${q.id_ayat}</button>
+                            <button type="button" class="btn btn-2 rounded-0">Tafsir</button>
+                            <button type="button" class="btn btn-3 rounded-0">Right</button>
                         </div>
-                        <div class="collapse" id="collapseExample2">
-                            <div class="card card-body my-3">
-                            bbbb pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                        <div class="collapse show" id="collapseExample">
+                            <div class="card card-body rounded-0 pr-2 shadow-sm m-0 pb-0 ">
+                            <p class="arabic" lang="ar" dir="rtl">${q.text} <span class="nmr-arabic">${q.id_ay_ar}</span></p>
+                            <p class="terjemah d-flex p-2"><span class="nr-ayat">${q.id_ayat+').'}</span> ${q.tr_id}</p>
                             </div>
                         </div>
                     </div>`;
         });
         $('.container-ayat').html(qcard);
+        ///
+
     },
     error : (e) => {
         console.log(e.responseText);
     }
 });
+
+
---
+++ src/js/judul.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $.getJSON("1.json", function(data){
+        console.log(data);
+        var dataQr = '';
+        $.each(data, function(key, value){
+            dataQr += '<h1 class="judul-arabic" lang="ar" dir="rtl">'+value.nm_ar+'</h1>';
+            dataQr += '<h5 class"p-0 mt-1">'+value.nm_id+'</h5>';
+            dataQr += '<h6 class"p-0 mt-1">'+'('+value.nm_tr+')'+'</h6>';
+            dataQr += '<p class"p-0 mt-1 keterangan">'+'Surah: '+value.id_surah+' | '+value.jm_ayat+' Ayat | '+value.type+'</p>';
+            
+        });
+        $('#judul').append(dataQr);
+        console.log(dataQr);
+    });
+});
