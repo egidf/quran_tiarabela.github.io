355fd62a7760b73dbb0408f5e37a69b1f1500ceb
Egi <egimalgina@gmail.com>
2021-02-02 05:30:44 +0700

    Menghapus file-file yang kurang perlu

--- src/js/data.js
+++ src/js/data.js
@@ -10,7 +10,7 @@ $.ajax ({
             qcard +=`<div class="container  my-3">
                         <div class="btn-group" role="group" aria-label="Basic">
                             <button id="btn-utama" type="button" class="btn btn-1 rounded-0" data-tfr="${q.tf_rk}" data-tfl="${q.tf_lk}" data-ids="${q.id_surah}" data-ida="${q.id_ayat}" data-textay="${q.text}">${q.id_surah} : ${q.id_ayat}</button>
-                            <button id="btn-sekunder" type="button" class="btn btn-2 rounded-0 tafsir-ringkas-button" data-bs-toggle="modal" data-bs-target="#exampleModal" data-tfr="${q.tf_rk}" data-tfl="${q.tf_lk}" data-ids="${q.id_surah}" data-ida="${q.id_ayat}" data-textay="${q.text}"><i class="fas fa-book"></i></button>
+                            <button id="btn-sekunder" type="button" class="btn btn-2 rounded-0 tafsir-ringkas-button" data-bs-toggle="modal" data-bs-target="#exampleModal" data-tfr="${q.tf_rk}" data-tfl="${q.tf_lk}" data-ids="${q.id_surah}" data-ida="${q.id_ayat}" data-textay="${q.text}" data-idar="${q.id_ay_ar}"><i class="fas fa-book-open"></i></button>
                             <button type="button" class="btn btn-3 rounded-0 fade" id="tafsir2">Tafsir Kemenag</button>
                         </div>
                         <div class="collapse show" id="collapseExample">
--- src/js/tafsir2.js
+++ src/js/tafsir2.js
@@ -1,31 +1,56 @@
-
-
-
-
 $(document).ready(function(){
-
-
-    
     
     $(".tafsir-ringkas-button").click(function(){
         const tfRingkas = $(this).data('tfr');
         const tfLengkap = $(this).data('tfl');
         const idS = $(this).data('ids');
         const idA = $(this).data('ida');
+        const idAr= $(this).data('idar');
         const textAyat = $(this).data('textay');
+        
 
-        pjudul = document.createTextNode('Tafsir ringkas surah: '+ idS+' ayat: '+ idA);
+        const pjudul = document.createTextNode('Kajian Surah: '+ idS+' ayat: '+ idA);
         $('.modal-title').append(pjudul);
 
-        pAyat = document.createTextNode(textAyat);
-        $('.lgi-1').append(pAyat);
+        const jTfringkas = document.createTextNode('Tafsir Ringkas Kemenag RI');
+        const pJtfRingkas = document.createElement('h6');
+        pJtfRingkas.append(jTfringkas);
+        pJtfRingkas.classList.add('judul-tafsir');
 
-        pTafsir = document.createTextNode(tfRingkas);
-        $('.tafsir-ringkas').append(pTafsir);
+        const jTflengkap = document.createTextNode('Tafsir Kemenag RI');
+        const pJtfLengkap = document.createElement('h6');
+        pJtfLengkap.append(jTflengkap);
+        pJtfLengkap.classList.add('judul-tafsir');
+        
+
+        const tAyat = document.createTextNode(textAyat +' '+ idAr);
+        const pAyat = document.createElement('p');
+        pAyat.append(tAyat);
+         $('.lgi-1').append(pAyat);
+ 
+        const tTafsirR = document.createTextNode(tfRingkas);
+        const pTafsirR = document.createElement('p');
+        pTafsirR.append(tTafsirR);
+        $('.tafsir-ringkas').append(pTafsirR);
+        $('.tafsir-ringkas').before(pJtfRingkas);
+
+        const tTafsirL = document.createTextNode(tfLengkap);
+        const pTafsirL = document.createElement('p');
+        pTafsirL.append(tTafsirL);
+        $('.tafsir-lengkap').append(pTafsirL);
+        $('.tafsir-lengkap').before(pJtfLengkap);
+
              $("#close").click(function(){
+                $('.judul-tafsir').remove();
                 $('.list-group-item').empty();
                 $('.modal-title').empty();
-        })
+            
      });
+        $(".btn-modal-tafsir").click(function(){
+            $('.judul-tafsir').remove();
+            $('.list-group-item').empty();
+            $('.modal-title').empty();
+})
   });
 
+});
